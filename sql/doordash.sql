5ca00eda664c8b8fe54d9a5d4ad683508f9b1327
shornc <126030805+shornc@users.noreply.github.com>
2026-02-23 21:44:52 +0000

    Remove duplicates and add CustomerId column

--- sql/doordash.sql
+++ sql/doordash.sql
@@ -11,3 +11,21 @@ FROM information_schema.columns
 WHERE table_schema = DATABASE()
 AND table_name = 'doordash';
 
+-- remove duplicates 
+CREATE TABLE temp_doordash AS SELECT DISTINCT * FROM doordash;
+
+DROP TABLE doordash;
+
+RENAME TABLE temp_doordash TO doordash;
+
+SELECT COUNT(*) FROM doordash;
+
+-- adding column `CustomerID` to the right 
+
+ALTER TABLE doordash 
+ADD COLUMN CustomerId INT AUTO_INCREMENT PRIMARY KEY;
+
+ALTER TABLE doordash
+MODIFY COLUMN CustomerId INT FIRST;
+
+SELECT * FROM doordash;
